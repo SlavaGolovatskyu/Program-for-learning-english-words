1755e6da1df541168e6de515769293f213e075d2
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-02-11 14:34:40 +0200

    Add files via upload

--- database/connect.py
+++ database/connect.py
@@ -1,25 +1,41 @@
+# imported "sqlite3" as a DataBase
+# imported library "random" for the generation random number
 import sqlite3
 import random
 
 class ManageDataBase:
-	def __init__(self):
+	def __init__(self): # def Initialization
+		# Connect to DB
 		self.db = sqlite3.connect('./database/sqlite.db')
+		# Take a cursor
 		self.s = self.db.cursor()
+		# Call Function
 		self.create_table_if_not_exists()
 
 	def create_table_if_not_exists(self):
+		# Create table if it is not there
 		self.s.execute("""CREATE TABLE IF NOT EXISTS 
 					  Words(_id INTEGER PRIMARY KEY AUTOINCREMENT,
 					  word TEXT)""")
 		self.db.commit()
 
+	# Get random word from DataBase
 	def get_random_word(self):
+		# Learn the number of elements in the DataBase
 		count = self.s.execute('SELECT COUNT(*) FROM Words').fetchone()[0]
 		if count == 1:
 			return self.s.execute('SELECT word FROM Words WHERE _id = (?)', (1,)).fetchone()[0]
 		return self.s.execute("""SELECT word FROM Words WHERE _id = (?)""",
 							 (random.randint(1, count + 1),)).fetchone()[0]
 
+	# Validation
+	def check_length_db(self) -> bool:
+		count = self.s.execute('SELECT COUNT(*) FROM Words').fetchone()[0]
+		if count == 0:
+			return False
+		return True
+
+	# Note word in DataBase
 	def write_word_in_db(self, word):
 		self.s.execute('INSERT INTO Words(word) VALUES (?)', (word,))
 		self.db.commit()
--- start_learn.py
+++ start_learn.py
@@ -1,21 +1,47 @@
+"""
+	* imported library configparser for the working with.ini files
+	* Tkinter for the create App
+	* database/connect.py imported for the working with DB
+""" 
 import configparser
 from tkinter import *
 from tkinter import messagebox
 from database.connect import ManageDataBase
 
-class WindowForStartLearn:
-	def __init__(self, parent):
-		self.config = configparser.ConfigParser()  # создаём объекта парсера
-		self.config.read("./options/options.ini") # Считываем данные с src/options.ini
+class Validation(object):
+	def __init__(self):
+		self.config = configparser.ConfigParser()  # create object parser's
+		self.config.read("./options/options.ini") # read data from ./options/options.ini
 
+	# 
+	def back_return_bool(self) -> bool:
+		if self.config['Work']['work'] == 'True':
+			messagebox.showerror('Ошибка', 'Остановите сначала програму. Затем вы сможете выйти.')
+			return False
+		else:
+			return True
+
+	# Stopped program if she working
+	# else send error.
+	def stop(self):
+		if self.config['Work']['work'] == 'True':
+			self.config['Work']['work'] = 'False'
+			messagebox.showinfo('Success', 'Програма успешно остановленна.')
+		else:
+			messagebox.showerror('Error', 'Режим изучения слов не включен.')
+
+class WindowForStartLearn(Validation):
+	def __init__(self, parent):
 		self.ManageDB = ManageDataBase()
 
-		self.width = 400
-		self.height = 300
+		# Width and Height APP
+		self.width, self.height = 400, 300
 
+		# Create self variable parent
 		self.main = parent
-		self.root = Toplevel(parent)
 
+		# Creating daughter window based on the father
+		self.root = Toplevel(parent)
 
 		self.root.geometry(f'{self.width}x{self.height}+300+300')
 		self.root.title('Learn English')
@@ -34,19 +60,18 @@ class WindowForStartLearn:
 							     bg = bg_for_buttons, fg = fg_for_buttons, command = self.back)
 		self.button_stop = Button(self.root, text = 'Остановить', width = '25', height = '2',
 							     bg = bg_for_buttons, fg = fg_for_buttons, command = self.stop)
+		self.button_start = Button(self.root, text = 'Запустить.', width = '25', height = '2',
+							      bg = bg_for_buttons, fg = fg_for_buttons, command = self.start_regime)
 
 		self.enter_3.bind('<Button-1>', self.delete_text)
 
+	def start_regime(self):
+
 	def stop(self):
-		if self.config['Work']['work'] == 'True':
-			self.config['Work']['work'] = 'False'
-		else:
-			messagebox.showerror('Error', 'Режим изучения слов не включен.')
+		super(WindowForStartLearn, self).stop()
 
 	def back(self):
-		if self.config['Work']['work'] == 'True':
-			messagebox.showerror('Ошибка', 'Остановите сначала програму. Затем вы сможете выйти.')
-		else:
+		if super(WindowForStartLearn, self).back_return_bool():
 			self.root.destroy()
 			self.main.deiconify()
 
