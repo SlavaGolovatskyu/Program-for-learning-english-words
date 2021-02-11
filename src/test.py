0b5d08019a77ca395d440d86966c7e7618b1031c
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-02-11 22:23:52 +0200

    Delete test.py

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import configparser  # импортируем библиотеку
-
-config = configparser.ConfigParser()  # создаём объекта парсера
-config.read("options.ini")  # читаем конфиг
-
-print(config["Work"]["start"])  # обращаемся как к обычному словарю!
